e7fe2e3be1c18ce7d35da966bf0fcd026a977b6a
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-07-08 16:23:00 -0300

    Salary updated

--- Proyecto final/salary.py
+++ Proyecto final/salary.py
@@ -18,6 +18,17 @@ def salary_data_by_location(data_frame, min_salary = 0, max_salary = 100000000, 
     valid_salary = data_frame[(data_frame['region'].isin(locations)) & (data_frame['Salario'] > min_salary) & (data_frame['Salario'] <= max_salary)]
     return valid_salary['Salario']
 
+def box_info(graph, data):
+    min_value = data.min()
+    max_value = data.max()
+    Q1 = data.quantile(0.25)
+    Q2 = data.quantile(0.5)
+    Q3 = data.quantile(0.75)
+    text = f'Mediana: {median(data)}\nModa: {mode(data)}\nMedia: {round(mean(data), 2)}\nMinimo: {min_value}\nMaximo: {max_value}\nCuartil Q1: {Q1}\nCuartil Q2: {Q2}\nCuartil Q3: {Q3}'
+    graph.text(0.04, 0.5, text, transform=graph.transAxes, fontsize=10,
+                 bbox=dict(facecolor='white', edgecolor='black', alpha=0.6),
+                 ha='left')
+
 def salary_graphics(data_frame):
     fig, ((hist1, hist2), (box1, box2)) = plt.subplots(2, 2, figsize=(12, 9))
     
@@ -33,30 +44,10 @@ def salary_graphics(data_frame):
     box2.boxplot(limited_data, showfliers=False)
     box2.set_title('Con tope de 300000 sin outliers')
     
-    min_value = limited_data.min()
-    max_value = limited_data.max()
-    Q1 = limited_data.quantile(0.25)
-    Q2 = limited_data.quantile(0.5)
-    Q3 = limited_data.quantile(0.75)
-    text = f'Mediana: {median(limited_data)}\nModa: {mode(limited_data)}\nMedia: {round(mean(limited_data), 2)}\nMinimo: {min_value}\nMaximo: {max_value}\nCuartil Q1: {Q1}\nCuartil Q2: {Q2}\nCuartil Q3: {Q3}'
-    box1.text(0.04, 0.5, text, transform=box1.transAxes, fontsize=10,
-                 bbox=dict(facecolor='white', edgecolor='black', alpha=0.6),
-                 ha='left')
-    
+    box_info(box1, limited_data)
     
     plt.tight_layout()
     plt.show()
-
-def box_info(graph, data):
-    min_value = data.min()
-    max_value = data.max()
-    Q1 = data.quantile(0.25)
-    Q2 = data.quantile(0.5)
-    Q3 = data.quantile(0.75)
-    text = f'Mediana: {median(data)}\nModa: {mode(data)}\nMedia: {round(mean(data), 2)}\nMinimo: {min_value}\nMaximo: {max_value}\nCuartil Q1: {Q1}\nCuartil Q2: {Q2}\nCuartil Q3: {Q3}'
-    graph.text(0.04, 0.5, text, transform=graph.transAxes, fontsize=10,
-                 bbox=dict(facecolor='white', edgecolor='black', alpha=0.6),
-                 ha='left')
 
 def salary_graphics_by_sex_and_locations(data_frame):
     fig, ((box1, box2), (box3, box4)) = plt.subplots(2, 2, figsize=(12, 9))
@@ -85,3 +76,4 @@ def salary_graphics_by_sex_and_locations(data_frame):
     
     plt.tight_layout()
     plt.show()
+    
